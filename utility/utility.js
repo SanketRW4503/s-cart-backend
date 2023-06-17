d4482d74c2915d295064989e81c599ef2e9f01ac
sanket waghmare <sanketwaghmare508@gmail.com>
2023-06-18 01:08:04 +0530

    cookie fixed

--- utility/utility.js
+++ utility/utility.js
@@ -5,8 +5,8 @@ const jwt = require('jsonwebtoken')
 function setCookie(user, res) {
     let token = jwt.sign({ payload: user._id },`${process.env.COOKIE_TOKEN_KEY}`);
     res.cookie('token', token, { maxAge: 1000 * 60 * 5, httpOnly: true ,
-     SameSite:'none',
-     secure:true,
+        sameSite: 'none', 
+        secure: true
     })
 
 }
