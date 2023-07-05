fc874625a2ddef3a87cf46afcfc87aa540b98164
sanket waghmare <sanketwaghmare508@gmail.com>
2023-07-05 13:46:18 +0530

    divede category for admin and user

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -26,76 +26,90 @@ async function set_category(req, res) {
 
 
 // GET all CATEGORY
-async function get_category(req,res) {
+async function get_category(req, res) {
     try {
 
         const allcollection = await categoryModel.find()
-        if(allcollection){
-            const products= await productModel.find();
-            if(products){
-                let items=[];
+        if (allcollection) {
+            const products = await productModel.find();
+            if (products) {
+                let items = [];
                 for (let i = 0; products.length !== i; i++) {
                     let data = allcollection.filter((p) => p.category == products[i].category)
                     items.push(data[0])
-                   
+
                 }
                 const categoryset = [...new Set(items)];
-           
+
                 res.json({ success: true, categoryset })
 
             }
         }
     } catch (error) {
 
-        res.json({ success: false, message: 'ERROR IS:'+error })
+        res.json({ success: false, message: 'ERROR IS:' + error })
     }
 }
 
 
-// delete category
-
-async function delete_category(req,res){
+// get category for admin
+async function get_admins_category(req, res) {
 
     try {
-        const result= await categoryModel.deleteOne({category:req.body.category});
-        if(result.deletedCount==1){
-            res.json({success:true,message:'Category Successfully Deleted !'})
-        }else{
-            res.json({success:false,message:'Category Not Found!'})
+        const allcollections = await categoryModel.find();
+        if (allcollections) {
+            res.json({ success: true, allcollections })
+        }
+    } catch (error) {
+        res.json({ success: false, messge: 'error' + error })
+    }
+
+}
+
+// delete category
+
+async function delete_category(req, res) {
+
+    try {
+        const result = await categoryModel.deleteOne({ category: req.body.category });
+        if (result.deletedCount == 1) {
+            res.json({ success: true, message: 'Category Successfully Deleted !' })
+        } else {
+            res.json({ success: false, message: 'Category Not Found!' })
 
         }
     } catch (error) {
-        res.json({success:false,error})
+        res.json({ success: false, error })
     }
 }
 
 
 // update category name
 
-async function update_category(req,res){
+async function update_category(req, res) {
 
     try {
-        const result= await categoryModel.findOne({category:req.body.category});
+        const result = await categoryModel.findOne({ category: req.body.category });
         console.log(result)
-        if(result!==null){
-            result.category=req.body.newcategory
+        if (result !== null) {
+            result.category = req.body.newcategory
             const status = await result.save();
-            if(status!==null){
-                res.json({success:true,message:'Category Updated Successfully !'})
-            }else{
-                res.json({success:true,message:'some error occur... try again !'})
+            if (status !== null) {
+                res.json({ success: true, message: 'Category Updated Successfully !' })
+            } else {
+                res.json({ success: true, message: 'some error occur... try again !' })
 
             }
-        
-        }else{
-            res.json({success:true,message:'requested category to update not found !'})
+
+        } else {
+            res.json({ success: true, message: 'requested category to update not found !' })
 
         }
-        
+
 
     } catch (error) {
-        res.json({success:false,error})
+        res.json({ success: false, error })
     }
 }
 
-export { set_category, get_category, delete_category, update_category };
+export { get_admins_category, set_category, get_category, delete_category, update_category };
--- routes/categoryRout.js
+++ routes/categoryRout.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { set_category, get_category, delete_category, update_category } from '../controllers/categoryController.js';
+import { set_category, get_category, delete_category, update_category  ,get_admins_category} from '../controllers/categoryController.js';
 import { is_this_admin } from '../utility/utility.js';
 
 const router = express.Router();
@@ -9,7 +9,11 @@ const router = express.Router();
 
 router.post('/set-category', is_this_admin, set_category);
 
+//this will return categories which are having products
 router.get('/get-category', get_category);
+
+//this will return all categories no matter its having products or not
+router.get('/admin/get-category', get_admins_category);
 
 router.post('/delete-category',is_this_admin,delete_category);
 
